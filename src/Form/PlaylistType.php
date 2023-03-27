a0139f37e8afbf43881690c4be8c67ad6048381a
FakiriYassine <yassineyassine0012@gmail.com>
2023-03-27 15:26:28 +0200

    MISSION 2 : Tâche 2 - Gérer les playlists

---
+++ src/Form/PlaylistType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Formation;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * Description of PlaylistType
+ *
+ * @author intad
+ */
+class PlaylistType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void {
+        $builder
+                ->add('name', TextType::class, [
+                    'label' => 'Playlist',
+                    'required' => true
+                ])
+                ->add('description', TextareaType::class, [
+                    'label' => 'Description',
+                    'required' => false
+                ])
+                ->add('formations', EntityType::class, [
+                    'class' => Formation::class,
+                    'choice_label' => 'title',
+                    'multiple' => true,
+                    'required' => false
+                ])
+                ->add('submit', SubmitType::class, [
+                    'label' => 'Enregistrer'
+        ]);
+    }
+
+}
